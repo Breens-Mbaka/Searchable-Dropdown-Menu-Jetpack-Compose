e12ae8c0a366bb46946c5cf999dad36968a92044
Breens <breens.mbaka@dlight.com>
2022-11-14 18:19:40 +0300

    Fix: Put the items to be displayed  inside a dropdown menu

--- app/src/main/java/com/breens/searchableexposeddropdownmenujetpackcompose/MainActivity.kt
+++ app/src/main/java/com/breens/searchableexposeddropdownmenujetpackcompose/MainActivity.kt
@@ -5,9 +5,12 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.breens.searchableexposeddropdownmenujetpackcompose.ui.theme.SearchableExposedDropDownMenuJetpackComposeTheme
@@ -18,29 +21,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SearchableExposedDropDownMenuJetpackComposeTheme {
-                Column(
-                    Modifier
-                        .fillMaxSize()
-                        .padding(16.dp)
-                ) {
-                    val sports =
-                        mutableListOf(
-                            "Basketball",
-                            "Rugby",
-                            "Football",
-                            "MMA",
-                            "Motorsport",
-                            "Snooker",
-                            "Tennis"
+                val sports = mutableListOf(
+                    "Basketball",
+                    "Rugby",
+                    "Football",
+                    "MMA",
+                    "Motorsport",
+                    "Snooker",
+                    "Tennis"
+                )
+                LazyColumn(modifier = Modifier.fillMaxSize(),contentPadding = PaddingValues(16.dp)) {
+                    item {
+                        SearchableExpandedDropDownMenu(
+                            listOfItems = sports,
+                            modifier = Modifier.fillMaxWidth(),
+                            onDropDownItemSelected = { item ->
+                                Toast.makeText(applicationContext, item, Toast.LENGTH_SHORT).show()
+                            }
                         )
-                    SearchableExpandedDropDownMenu(
-                        listOfItems = sports,
-                        modifier = Modifier.fillMaxWidth(),
-                        onDropDownItemSelected = { item ->
-                            Toast.makeText(applicationContext, item, Toast.LENGTH_SHORT).show()
-                        }
-                    )
+                    }
                 }
+//                Column(modifier = Modifier
+//                    .padding(16.dp)
+//                    .fillMaxHeight()) {
+//                    SearchableExpandedDropDownMenu(
+//                        listOfItems = sports,
+//                        modifier = Modifier.fillMaxWidth(),
+//                        onDropDownItemSelected = { item ->
+//                            Toast.makeText(applicationContext, item, Toast.LENGTH_SHORT).show()
+//                        }
+//                    )
+//                }
             }
         }
     }
